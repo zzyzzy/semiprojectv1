0380f2732276b6957d6c5f207d05c24a8c048be2
zzyzzy <zzyzigy@gmail.com>
2025-02-20 16:52:30 +0900

    nav에 정의된 링크에 맞게 컨트롤러와 엔드포인트 정의 엔드포인트에 따른 뷰 페이지도 지정

---
+++ src/main/java/com/example/zzyzzy/semiprojectv1/controller/GalleryController.java
@@ -0,0 +1,16 @@
+package com.example.zzyzzy.semiprojectv1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/gallery")
+public class GalleryController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "views/gallery/list";
+    }
+
+}
---
+++ src/main/java/com/example/zzyzzy/semiprojectv1/controller/MemberController.java
@@ -0,0 +1,26 @@
+package com.example.zzyzzy.semiprojectv1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/join")
+    public String join() {
+        return "views/member/join";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "views/member/login";
+    }
+
+    @GetMapping("/myinfo")
+    public String myinfo() {
+        return "views/member/myinfo";
+    }
+
+}
