57375f7f2d942ae3161022877d6b74b74abab451
zzyzzy <zzyzigy@gmail.com>
2025-02-21 10:34:30 +0900

    회원가입 유효성 검사 1 - onXxx 방식

--- src/main/java/com/example/zzyzzy/semiprojectv1/controller/MemberController.java
+++ src/main/java/com/example/zzyzzy/semiprojectv1/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.example.zzyzzy.semiprojectv1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +12,11 @@ public class MemberController {
     @GetMapping("/join")
     public String join() {
         return "views/member/join";
+    }
+
+    @PostMapping("/join")
+    public String joinok() {
+        return "redirect:/member/login";
     }
 
     @GetMapping("/login")
