17527d93744cb5592dd63605238483e0a3c48ba9
zzyzzy <zzyzigy@gmail.com>
2025-03-04 10:31:08 +0900

    게시판 글쓰기 구현 1 - 글쓰기 폼 작성 google recaptcha 추가 tiny mce 추가

--- src/main/java/com/example/zzyzzy/semiprojectv1/controller/BoardController.java
+++ src/main/java/com/example/zzyzzy/semiprojectv1/controller/BoardController.java
@@ -61,4 +61,10 @@ public class BoardController {
         return "views/board/view";
     }
 
+
+    @GetMapping("/write")
+    public String write() {
+        return "views/board/write";
+    }
+
 }
