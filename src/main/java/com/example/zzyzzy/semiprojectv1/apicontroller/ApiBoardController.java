462d31045c448e90dab4401e4d2f85f37c21b09c
zzyzzy <zzyzigy@gmail.com>
2025-03-25 10:14:36 +0900

    게시판 목록 데이터 조회를 위한 API 구현 클라이언트 사이드 렌더링 준비

---
+++ src/main/java/com/example/zzyzzy/semiprojectv1/apicontroller/ApiBoardController.java
@@ -0,0 +1,27 @@
+package com.example.zzyzzy.semiprojectv1.apicontroller;
+
+import com.example.zzyzzy.semiprojectv1.domain.BoardListDTO;
+import com.example.zzyzzy.semiprojectv1.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/board")
+@RequiredArgsConstructor
+public class ApiBoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public ResponseEntity<BoardListDTO> list(@RequestParam(defaultValue = "1") int cpg) {
+        BoardListDTO boardListDTO = boardService.readBoard(cpg);
+
+        return new ResponseEntity<>(boardListDTO, HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/example/zzyzzy/semiprojectv1/controller/BoardController.java
+++ src/main/java/com/example/zzyzzy/semiprojectv1/controller/BoardController.java
@@ -29,22 +29,13 @@ public class BoardController {
     private final GoogleRecaptchaService googleRecaptchaService;
 
     @GetMapping("/list")
-    public String list(Model m, @RequestParam(defaultValue = "1") int cpg,
-                       HttpServletResponse response) {
+    public String list(HttpServletResponse response) {
         // 클라이언트 캐시 제어
         response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         response.setHeader("Pragma", "no-cache");
         response.setDateHeader("Expires", 0);
 
-        // RequestParam에 defaultValue를 이용하면
-        // cpg 매개변수가 전달되지 않을 경우 기본값인 1이 전달됨
         log.info("board/list 호출!!");
-
-        m.addAttribute("bdsdto", boardService.readBoard(cpg));
-        // m.addAttribute("bds", boardService.readBoard(cpg));
-        // m.addAttribute("cpg", cpg);
-        // m.addAttribute("stblk", ((cpg - 1) / 10) * 10 + 1);
-        // m.addAttribute("cntpg", boardService.countBoard());
 
         return "views/board/list";
     }
